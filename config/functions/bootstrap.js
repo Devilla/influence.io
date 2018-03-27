f0125c053ea8b8b40bfa5a1e77e47eb0d3bb8f77
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2018-03-27 21:56:39 +0530

    Start socket server on /web path

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -8,6 +8,20 @@
  * run jobs, or perform some special logic.
  */
 
+const Websocket = require('ws');
+
 module.exports = cb => {
+  const wss = new Websocket.Server({server: strapi.server, path: strapi.config.socketsServerPath});
+
+  // listen for user connection
+  wss.on('connection', function(ws){
+    console.log('Socket user connected!');
+    
+    // listen for user diconnect
+    ws.on('disconnect', () => console.log('Socket user disconnected!'));
+  });
+  
+  strapi.wss = wss; // register websockets inside strapi main object to use it globally anywhere
+  
   cb();
 };
