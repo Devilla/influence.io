413bba165ce8ee696ce347175de13fcc5e5f58a6
Maek Twain <saranshisat@icloud.com>
2018-03-29 03:45:06 +0530

    Websockets

---
+++ api/websocket/services/Websocket.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * `Websocket` service.
+ */
+
+
+module.exports = () => {
+
+  
+};
+
+
+
--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -8,8 +8,38 @@
  * run jobs, or perform some special logic.
  */
 
+const Websocket = require('ws');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
 
 module.exports = cb => {
-  
+  const wsServer = new Websocket.Server({server: strapi.server, path: strapi.config.socketsServerPath});
+
+  const eventHandler = function() {
+    wsServer.on('connection', function(ws){
+      console.log('Socket user connected!');
+      ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        strapi.message = message;
+        console.log("Here is the message",strapi.message);
+        /
+        //Send message to ES
+      });
+      // listen for user diconnect
+      ws.on('disconnect', () => console.log('Socket user disconnected!'));
+    });
+  };
+
+  eventHandler();
+
+  /**
+   * Pass the data from here
+   *
+   */
+
+
+  strapi.websocket = wsServer;
+
   cb();
 };
