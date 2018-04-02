09d001388cffaa8b1a45945c6ca810e3aba8d344
Maek Twain <saranshisat@icloud.com>
2018-04-02 18:54:49 +0530

    ES2

--- api/elasticsearch/services/Elasticsearch.js
+++ api/elasticsearch/services/Elasticsearch.js
@@ -5,11 +5,30 @@
  *
  * @description: A set of functions similar to controller's actions to avoid code duplication.
  */
+const elasticsearch = require('elasticsearch');
+
+
+const client = elasticsearch.Client({
+  host: strapi.config.elasticsearchNode,
+  log: 'trace',
+});
+
+
+client.ping({
+  requestTimeout: 1000
+},function (error) {
+  if(error){
+    strapi.log.info('Elasticsearch has some problem but its okay ', error);
+
+  }else {
+    strapi.log.info('ES has started' );
+  }
+
+});
 
 module.exports = {
 
   health : async () => {
-    let client = strapi.es;
     return new Promise((resolve, reject)=> {
       client.cluster.health({}, function (err,resp,status) {
         if(err) reject(err);
@@ -23,7 +42,6 @@ module.exports = {
 
 
   searchWithQuery: async (index,q) => {
-    let client = strapi.es;
     return new Promise((resolve, reject)=> {
       client.search({index: index, q: q}, function (err,resp,status) {
         if (err) reject(err);
--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -7,78 +7,11 @@
  * This gives you an opportunity to set up your data model,
  * run jobs, or perform some special logic.
  */
-const elasticsearch = require('elasticsearch');
+
 const Websocket = require('ws');
 
 
-const client = elasticsearch.Client({
-  host: strapi.config.elasticsearchNode,
-  log: 'trace',
-});
-
-
-client.ping({
-  requestTimeout: 1000
-},function (error) {
-  if(error){
-    strapi.log.info('Elasticsearch has some problem but its okay ', error);
-
-  }else {
-    strapi.log.info('ES has started ' );
-  }
-
-});
-
-client.indices.get({
-  index: 'clientwebsitedata',
-},function (err, resp, status) {
-  if(err) {
-    strapi.log.info(err);
-    //
-    createIndex();
-    //define Mappin
-  }else {
-    strapi.log.info('get', resp);
-  }
-});
-
-function createIndex() {
-  client.indices.create({
-    index: 'clientwebsitedata',
-    type: 'logs'
-  },function(err,resp,status) {
-    if(err) {
-      console.log(err);
-    }
-    else {
-      console.log('create',resp);
-    }
-  });
-}
-
 module.exports = async cb => {
-  // const pluginStore = strapi.store({
-  //   environment: strapi.config.environment,
-  //   type: 'plugin',
-  //   name: 'elasticsearch'
-  // });
-  //
-  // if (!await pluginStore.get({key: 'application'})) {
-  //   const value = {
-  //     name: 'ElasticSearch',
-  //     description: 'This API is going to be awesome with elasticsearch!'
-  //   };
-  //
-  //   await pluginStore.set({key: 'elasticsearch', value});
-  // }
-
-  //Create index if does not exist
-
-  //Get client
-
-
-
-
 
   const wsServer = new Websocket.Server({server: strapi.server, path: strapi.config.socketsServerPath});
 
@@ -112,13 +45,9 @@ module.exports = async cb => {
 
   eventHandler();
 
-
-  strapi.es = client;
-
   strapi.websocket = wsServer;
 
 
   cb();
 
 };
->>>>>>> ES1
