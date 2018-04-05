b0d533c3d2340ce3c9625016702ad277baa0c1b4
Shanky Rana <shankyrana@hotmail.com>
2018-04-06 01:45:19 +0530

    backend-url changed

--- public/frontend/src/services/api.js
+++ public/frontend/src/services/api.js
@@ -2,8 +2,8 @@ import { call, put, select } from 'redux-saga/effects';
 
 export const base =
   process.env.NODE_ENV === 'production'
-    ? 'http://localhost:1337/'
-    : 'http://localhost:1337/'; // eslint-disable-line
+    ? 'http://strapi.useinfluence.co/'
+    : 'http://strapi.useinfluence.co/'; // eslint-disable-line
 
 // const getToken = state => state.getIn(['auth', 'token']);//state.auth.token;
 
