308a323fc3fa586f9bb7b45d45909efd833d1ee1
Maek Twain <saranshisat@icloud.com>
2018-04-10 10:12:54 +0000

    Update api.js

--- public/frontend/src/services/api.js
+++ public/frontend/src/services/api.js
@@ -3,7 +3,7 @@ import { call, put, select } from 'redux-saga/effects';
 export const base =
   process.env.NODE_ENV === 'production'
     ? 'http://localhost:1337/'
-    : 'http://localhost:1337/' // eslint-disable-line
+    : 'http://localhost:1337/'; // eslint-disable-line
     : 'http://strapi.useinfluence.co/';
 
 // const getToken = state => state.getIn(['auth', 'token']);//state.auth.token;
