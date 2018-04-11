5293a6dca45b787f8abadf694b714d747a60c54c
Shanky Rana <shankyrana@hotmail.com>
2018-04-11 23:55:57 +0530

    new changes added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -59,11 +59,9 @@ module.exports = {
     staging: {
       user: TARGET_SERVER_USER,
       host: TARGET_SERVER_HOST,
-      ref: 'origin/master',
-      repo: REPO,
       path: TARGET_FRONTEND_APP_PATH,
       ssh_options: 'StrictHostKeyChecking=no',
-      'post-deploy': 'cd /public/frontend && npm install && pm2 startOrRestart ecosystem.config.js  --only frontend --env production'
+      'post-deploy': 'cd public/frontend && npm install && pm2 startOrRestart ecosystem.config.js  --only frontend --env production'
     },
   }
 };
