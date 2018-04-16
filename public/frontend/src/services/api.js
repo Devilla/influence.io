94fba6f17caef5e9ce4537b4ea4e1f531a716bdd
Maek Twain <saranshisat@icloud.com>
2018-04-16 16:44:28 +0530

    Added if condition for plans

--- public/frontend/src/services/api.js
+++ public/frontend/src/services/api.js
@@ -10,6 +10,13 @@ export const base =
 // const base = `${base}`;
 
 export const GET = (url) => {
+  if (url.valueOf('plan')){
+    return fetch('strapi.useinfluence.co' + url, {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(res => res)
+  }
   const token = JSON.parse(localStorage.getItem('authToken')).token;
 
   return fetch(base + url, {
