e9b672dfc4f983799abbc055260adc2f27fce21d
Shanky Rana <shankyrana@hotmail.com>
2018-05-22 19:44:06 +0530

    campaign creation issue resolved

--- api/campaign/services/Campaign.js
+++ api/campaign/services/Campaign.js
@@ -170,8 +170,7 @@ module.exports = {
     if(dom.error) {
       return dom;
     } else {
-			const data = await Campaign.create(_.omit(values, _.keys(_.groupBy(strapi.models.campaign.associations, 'alias'))));
-			await strapi.hook.mongoose.manageRelations('campaign', _.merge(_.clone(data), { values }));
+			const data = await Campaign.create(values);
 
 			await Notificationtypes.find()
       .exec()
