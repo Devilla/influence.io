c74f0b0deb2dda83228d0d9db3a0295d32b4c9b3
Shanky Rana <shankyrana@hotmail.com>
2018-07-11 12:22:29 +0530

    enrichment issue resolved

--- api/elasticsearch/services/Elasticsearch.js
+++ api/elasticsearch/services/Elasticsearch.js
@@ -29,9 +29,9 @@ let getUser = async function(email, callback) {
         callback(null, res);
       });
     } catch(err) {
-      console.log(email, '===============email');
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       userDetail = {
-        username: email?email.replace(/@.*$/,""):'Anonymous'
+        username: re.test(email)?email.replace(/@.*$/,""):'Anonymous'
       };
       callback(null, userDetail);
     }
