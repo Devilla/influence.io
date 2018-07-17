134a1a49b9e0ead204c03400bc19743d561534cb
devilla <dev.koold@gmail.com>
2018-07-17 17:00:58 +0530

    /api/coupon tests : add,fetchAll, fetchCode, fetch, edit, remove

---
+++ test/functional_test/coupon.test.js
@@ -0,0 +1,119 @@
+/**
+ * Test picasaWeb and Gravatar
+ * @type {"assert".internal | ((value: any, message?: string) => void)}
+ */
+let chai = require('chai');
+let expect = chai.expect;;
+const coupon = require('../../api/coupon/services/Coupon');
+
+const websiteUrl='';
+const isActive=false;
+describe('Should Return coupon Data', function() {
+  describe('#coupon-add()', function() {
+    it('should return data when the value is passed', function() {
+      let values = {
+        name:'Raman Parashar',
+        email:"raman.parashar.dce@gmail.com"
+      };
+        coupon.add(values).then(function (data) {
+         expect(data.name).to.equal('Raman Parashar');
+         expect(data.email).to.equal('raman.parashar.dce@gmail.com');
+      });
+    });
+  });
+});
+
+
+describe('Should Return coupon Data', function() {
+  describe('#fetchAll()', function() {
+    it('should return data when the value is passed', function() {
+      let params = {
+        websiteLive:'Useinfluence.co',
+        notificationCount:34,
+        uniqueUsers:12
+      }
+        coupon.fetchAll(params).then(function (data) {
+         expect(data.websiteLive).to.notequal(null);
+         expect(data.notificationCount).to.notequal(null);
+         expect(data.uniqueUsers).to.notequal(null);
+
+      });
+    });
+  });
+});
+
+describe('Should Return coupon Data', function() {
+  describe('#fetchCode()', function() {
+    it('should return data when the value is passed', function() {
+      let params = {
+        websiteLive:'Useinfluence.co',
+        notificationCount:34,
+        uniqueUsers:12
+      }
+        coupon.fetchCode(params).then(function (data) {
+         expect(data.websiteLive).to.notequal(null);
+         expect(data.notificationCount).to.notequal(null);
+         expect(data.uniqueUsers).to.notequal(null);
+
+      });
+    });
+  });
+});
+
+describe('Should Return coupon Data', function() {
+  describe('#fetch()', function() {
+    it('should return data when the value is passed', function() {
+      let params = {
+        websiteLive:'Useinfluence.co',
+        notificationCount:34,
+        uniqueUsers:12
+      }
+        coupon.fetch(params).then(function (data) {
+         expect(data.websiteLive).to.notequal(null);
+         expect(data.notificationCount).to.notequal(null);
+         expect(data.uniqueUsers).to.notequal(null);
+
+      });
+    });
+  });
+});
+
+describe('Should Edit coupon Data', function() {
+  describe('#edit()', function() {
+    it('should return data when the value is passed', function() {
+      let params = {
+        websiteLive:'Useinfluence.co',
+        notificationCount:34,
+        uniqueUsers:12
+      };
+      let values = {
+        name:'Raman Parashar',
+        email:"raman.parashar.dce@gmail.com"
+      };
+        coupon.edit(params,values).then(function (data) {
+         expect(data.websiteLive).to.notequal(null);
+         expect(data.notificationCount).to.notequal(null);
+         expect(data.uniqueUsers).to.notequal(null);
+         expect(data.name).to.notequal(null);
+         expect(data.email).to.notequal(null);
+        });
+    });
+  });
+});
+
+describe('Should Edit coupon Data', function() {
+  describe('#remove()', function() {
+    it('should return data when the value is passed', function() {
+      let params = {
+        websiteLive:'Useinfluence.co',
+        notificationCount:34,
+        uniqueUsers:12
+      };
+        coupon.remove(params).then(function (data) {
+         expect(data.websiteLive).to.notequal(null);
+         expect(data.notificationCount).to.notequal(null);
+         expect(data.uniqueUsers).to.notequal(null);
+        });
+    });
+  });
+});
