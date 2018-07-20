f1b03141f695ab53751ad5513b65cfd82d527336
devilla <dev.koold@gmail.com>
2018-07-20 20:09:59 +0530

    test corrections and commented : run single test suite at a time

--- test/functional/websocket.test.js
+++ test/functional/websocket.test.js
@@ -1,36 +1,35 @@
-/**
- *
- * @type {"assert".internal | ((value: any, message?: string) => void)}
- */
-
-let chai = require('chai');
-let expect = chai.expect;
-const websocket = require('../../api/websocket/services/Websocket');
-
-
-describe('Should Return websocket Data', function() {
-  describe('#log()', function() {
-    it('should return data when the value is passed', function() {
-      let logs = 'test_string';
-      const data =  websocket.log(logs);
-         expect(data).to.not.equal(null);
-    });
-  });
-});
-
-describe('Should Return websocket Data', function() {
-  describe('#health()', function() {
-    it('should return data when the value is passed', function() {
-      let params = {
-        id:'5b3b49c8341d70505653d750',
-        websiteLive:'Useinfluence.co',
-        notificationCount:34,
-        uniqueUsers:12
-      }
-        const data= websocket.health(params);
-         expect(data).to.not.equal(null);
-
-
-    });
-  });
-});
+// /**
+//  *
+//  * @type {"assert".internal | ((value: any, message?: string) => void)}
+//  */
+//
+// let chai = require('chai');
+// let expect = chai.expect;
+// const websocket = require('../../api/websocket/services/Websocket');
+//
+//
+// describe('Should Return websocket Data', function() {
+//   describe('#log()', function() {
+//     it('should return data when the value is passed', function() {
+//       let params = {
+//         args:'su7f0b1764'
+//       }
+//       const data =  websocket.log(params);
+//          expect(data).to.not.equal(null);
+//     });
+//   });
+// });
+//
+// describe('Should Return websocket Data', function() {
+//   describe('#health()', function() {
+//     it('should return data when the value is passed', function() {
+//       let params = {
+//         args:'su7f0b1764'
+//       }
+//         const data= websocket.health(params);
+//          expect(data).to.not.equal(null);
+//
+//
+//     });
+//   });
+// });
