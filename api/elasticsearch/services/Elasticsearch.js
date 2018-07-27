333471de78ee666c2719d128679281189d7b7f67
Shanky Rana <shankyrana@hotmail.com>
2018-07-27 10:51:43 +0530

    testing es

--- api/elasticsearch/services/Elasticsearch.js
+++ api/elasticsearch/services/Elasticsearch.js
@@ -57,6 +57,8 @@ let logUser = async function(query) {
   if(response.aggregations && response.aggregations.users.buckets.length) {
     await response.aggregations.users.buckets.map(details => {
       details = details.user_docs.hits.hits[0];
+      console.log(details);
+
       let form = details._source.json.value.form;
       let email = form.email || form.EMAIL || form.Email;
       let timestamp = details._source.json.value.timestamp;
