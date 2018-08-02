a0439136096d092e13cafa9a45a30dbfa3f8bd3c
devilla <dev.koold@gmail.com>
2018-08-02 11:21:10 +0530

    initial commit

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,5 @@ if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
 } else {
   // we are in development - return dev keys!!!
-  module.exports = require('./dev');
+  module.exports = require('./prod');
 }
